52be80f3248e16b1379d7d19fdf1b59597420e57
joshualead <53381449+joshualead@users.noreply.github.com>
2022-10-27 01:41:55 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,5 @@
 const QUERIES = {
-  INSERT_ADMIN_TABLE:
-    "INSERT INTO admin_table (user_id, name) VALUES (?,?)",
+  INSERT_ADMIN_TABLE: "INSERT INTO admin_table (user_id, name) VALUES (?,?)",
   INSERT_TRAINER_TABLE:
     "INSERT INTO trainer_table (user_id, name, course_id) VALUES (?,?,?);",
   INSERT_TRAINEE_TABLE:
@@ -15,23 +14,23 @@ const QUERIES = {
     "INSERT INTO score_table (trainee_id, course_id, task_id, score) VALUES (?,?,?,?);",
   INSERT_SUBMISSION_TABLE:
     "INSERT INTO submission_table (trainee_id, task_id, course_id, content, date) VALUES (?,?,?,?,?);",
-  INSERT_REFRESH_TOKEN:
-    "INSERT INTO refreshToken_table (refreshToken) VALUES (?);",
   UPDATE_ASSIGN_STATUS_DATE:
     "UPDATE course_table SET assigned_status = ?, date = ?  WHERE course_id = ?;",
+  UPDATE_TRAINER_ASSIGNED_COURSE:
+    "UPDATE trainer_table SET course_id = NULL WHERE user_id = ?;",
   UPDATE_ASSIGN_STATUS:
     "UPDATE course_table SET assigned_status = ?  WHERE course_id = ?;",
-  UPDATE_ASSIGNED_COURSE:"UPDATE trainer_table SET course_id = ?  WHERE user_id = ?;",
+  UPDATE_ASSIGNED_COURSE:
+    "UPDATE trainer_table SET course_id = ?  WHERE user_id = ?;",
   UPDATE_SCORE: "UPDATE score_table SET score=? WHERE score_id = ?",
   UPDATE_PASSWORD:
     "UPDATE credentials_table SET password = ? WHERE user_id = ?",
-  SELECT_REFRESH_TABLE:
-    "SELECT refreshToken FROM refreshToken_table WHERE refreshToken=?",
   SELECT_ADMIN_TABLE: "SELECT * FROM admin_table",
   SELECT_TRAINER_TABLE: "SELECT * FROM trainer_table",
   SELECT_TRAINEE_TABLE: "SELECT * FROM trainee_table",
   SELECT_COURSE_TABLE: "SELECT * FROM course_table",
-  GET_COURSE_STATUS:"SELECT assigned_status FROM course_table WHERE course_id = ?",
+  GET_COURSE_STATUS:
+    "SELECT assigned_status FROM course_table WHERE course_id = ?",
   SELECT_TRAINER_TABLE_SPEC:
     "SELECT trainer_table1.user_id AS user_id, trainer_table1.name AS name, credentials_table.email AS email FROM (SELECT * FROM trainer_table WHERE user_id = ?) AS trainer_table1 INNER JOIN credentials_table ON trainer_table1.user_id = credentials_table.user_id ",
   SELECT_TRAINEE_TABLE_SPEC:
@@ -44,8 +43,6 @@ const QUERIES = {
     "SELECT task_id from task_table WHERE task_name=? AND description=? AND course_id=?",
   SELECT_SUBMISSION_TABLE:
     "SELECT * FROM submission_table WHERE trainee_id= ? AND course_id= ? AND task_id=?",
-  SELECT_SCORE_TABLE:
-    "SELECT * FROM score_table WHERE task_id=? AND course_id=?",
   SELECT_SINGLE_SCORE: "SELECT * FROM score_table WHERE score_id=?",
   SELECT_JOIN_SCORE_NAME: `SELECT score_table1.score_id AS score_id, score_table1.trainee_id AS trainee_id, trainee_table.name AS trainee_name, score_table1.score AS score  FROM 
     (SELECT * FROM score_table WHERE task_id=? AND course_id=?) AS score_table1 INNER JOIN 
@@ -57,23 +54,17 @@ const QUERIES = {
     "SELECT assigned_status,count(*) as status_details_count FROM course_table GROUP BY assigned_status",
   SELECT_COMPLETED_COURSES:
     "SELECT * FROM course_table WHERE assigned_status = 2 OR assigned_status = 3 ORDER BY date DESC",
-  SELECT_UNASSIGNED_COURSES:
-    "SELECT * FROM course_table WHERE assigned_status = 0",
   SELECT_AVG_SCORES:
     "SELECT trainee_id,AVG(score) AS avgmarks FROM score_table WHERE course_id=? GROUP BY trainee_id",
-  GET_TRAINER_WCID : "SELECT * FROM trainer_table WHERE course_id=?",
-
+  CHECK_IF_COURSE_IN_TCMAP: "SELECT DISTINCT(course_id) FROM tcmap_table;",
+  GET_TRAINER_WCID: "SELECT * FROM trainer_table WHERE course_id=?",
   SELECT_COURSE_TAKEN: "SELECT course_id from trainer_table WHERE user_id = ?",
   DELETE_COURSE: "DELETE FROM course_table WHERE course_id=?",
   DELETE_ADMIN: "DELETE FROM admin_table WHERE user_id=?",
   DELETE_TRAINER: "DELETE FROM trainer_table WHERE user_id=?",
-  DELETE_TASK_WCID : "DELETE FROM task_table WHERE course_id=?",
-  DELETE_SUBMISSION_WCID : "DELETE FROM submission_table WHERE course_id=?",
-  DELETE_SCORE_WCID : "DELETE FROM score_table WHERE course_id=?",
   DELETE_TRAINEE: "DELETE FROM trainee_table WHERE user_id=?",
   DELETE_TASK: "DELETE FROM task_table WHERE task_id=?",
   DELETE_SCORE: "DELETE FROM score_table WHERE task_id=?",
-  DELETE_REFRESH_TOKEN: "DELETE FROM refreshToken_table WHERE refreshToken=?",
   DELETE_TOKEN: "DELETE FROM token_table WHERE token=?",
   CHECK_CRED_ID:
     "SELECT EXISTS(SELECT * from credentials_table WHERE user_id=?) AS v;",
@@ -83,6 +74,5 @@ const QUERIES = {
     "INSERT INTO credentials_table (user_id,password,email,type) VALUES (?,'$2b$10$r.HQlbkidfFMRNCEnpco1e4DGVXzhCPDO4iEHFk5KPMZLeN4IuQ6C',?,?)",
   DELETE_CRED_TABLE: "DELETE FROM credentials_table WHERE user_id=?",
   SELECT_TOKEN: "SELECT * FROM token_table WHERE token=?",
-  CHECK_IF_MAIL:"SELECT * from credentials_table WHERE email=?"
 };
 module.exports = QUERIES;
